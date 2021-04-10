5a78a2d2c35903ded788c9ce09243c811db06917
Armando Chacon <jarmando.chacon36@gmail.com>
2021-04-10 15:15:58 -0500

    - Added project structure packages

--- app/src/main/java/com/example/testmercadolibre/activity/MainActivity.kt
+++ app/src/main/java/com/example/testmercadolibre/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.testmercadolibre
+package com.example.testmercadolibre.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testmercadolibre.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
